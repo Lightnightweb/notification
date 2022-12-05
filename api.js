fb265c0406c7da828f804ce6802110c3eb364df5
Lightnightweb <110893762+Lightnightweb@users.noreply.github.com>
2022-12-05 16:45:22 +0900

    Add files via upload

---
+++ api.js
@@ -0,0 +1,28 @@
+class api {
+    getInfo() {
+      return {
+        id: 'api', // change this if you make an actual extension!
+        name: 'api',
+        blocks: [
+          {
+            opcode: 'api',
+            blockType: Scratch.BlockType.BOOLEAN,
+            text: '[ONE]を取得',
+            arguments: {
+              ONE: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue:'https://api-public.odpt.org/api/v4/odpt:Bus?odpt:operator=odpt.Operator:Toei'
+              },
+            }
+          }
+        ]
+      };
+    }
+    strictlyEquals(args) {
+      fetch(args.ONE)
+        .then(response=>{
+            return response.json();
+        })
+    }
+  }
+  Scratch.extensions.register(new api());
