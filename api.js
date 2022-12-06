a525173249ffed56d97364d972e551f9724b4748
Lightnightweb <110893762+Lightnightweb@users.noreply.github.com>
2022-12-06 13:45:44 +0900

    Update api.js

--- api.js
+++ api.js
@@ -1,13 +1,13 @@
 document.createElement("script")
 script.src="https://code.jquery.com/jquery-3.0.0.min.js";
-class api {
+class aaaaaa {
     getInfo() {
       return {
-        id: 'api', // change this if you make an actual extension!
-        name: 'api',
+        id: 'aaaaaa', // change this if you make an actual extension!
+        name: 'aaaaaa',
         blocks: [
           {
-            opcode: 'api',
+            opcode: 'aaaaaa',
             blockType: Scratch.BlockType.REPORTER,
             text: '[ONE]を取得',
             arguments: {
@@ -20,7 +20,7 @@ class api {
         ]
       };
     }
-    api(args) {
+    aaaaaa(args) {
       fetch(args.ONE)
         .then(Response=>{
           return Response.json
@@ -30,4 +30,4 @@ class api {
       })
    }  
 }
-Scratch.extensions.register(new api());
+Scratch.extensions.register(new aaaaaa());
