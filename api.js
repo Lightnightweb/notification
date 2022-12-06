cb7302019d8dc0778a446648f1754929af307be2
Lightnightweb <110893762+Lightnightweb@users.noreply.github.com>
2022-12-06 14:49:06 +0900

    Add files via upload

--- api.js
+++ api.js
@@ -22,7 +22,7 @@ class apisyutoku {
         return fetch(args.ONE)
         .then((response)=>{
           let res=response.json
-          return JSON.stringify(res)
+          return JSON.parse(res)
       })
    }
 }
