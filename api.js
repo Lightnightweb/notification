3d122d648fdf3b5093db8256c9907ffe2e94fe5f
Lightnightweb <110893762+Lightnightweb@users.noreply.github.com>
2022-12-06 13:43:04 +0900

    Add files via upload

--- api.js
+++ api.js
@@ -1,4 +1,4 @@
-let script=document.createElement("script");
+document.createElement("script")
 script.src="https://code.jquery.com/jquery-3.0.0.min.js";
 class api {
     getInfo() {
@@ -24,9 +24,10 @@ class api {
       fetch(args.ONE)
         .then(Response=>{
           return Response.json
+        .catch(error=>{
+          return ("失敗")
+        })
       })
-      }
-      
-    }
-
-  Scratch.extensions.register(new api());
+   }  
+}
+Scratch.extensions.register(new api());
