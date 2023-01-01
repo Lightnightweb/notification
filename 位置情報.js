60cc1f26bfc57f103a11a0d5cb84c9cb970449a3
Lightnightweb <110893762+Lightnightweb@users.noreply.github.com>
2023-01-01 11:48:35 +0900

    Add files via upload

--- 位置情報.js
+++ 位置情報.js
@@ -5,31 +5,23 @@ class StrictEqualityExtension {
         name: '位置情報',
         blocks: [
           {
-            opcode: '1',
-            blockType: Scratch.BlockType.REPORTER,
-            text: '位置情報(経度)',
-          },
-          {
             opcode: '2',
             blockType: Scratch.BlockType.REPORTER,
-            text: '位置情報(緯度)',
+            text: '位置情報',
           }
         ]
       };
     }
-    1(){      
-      function ichi2(position) {
-        return position.coords.longitude
-      }
-      let a = navigator.geolocation.getCurrentPosition(ichi2)
-      return a
-    }
     2(){   
-      function ichi2(position) {
-        return position.coords.latitude
+      function success(position) {
+        let a= ("緯度:" + position.coords.latitude);
+        a= (a+"経度:" + position.coords.longitude);
+        return a
       }
-      let a= navigator.geolocation.getCurrentPosition(ichi2)
-      return a
+      function error() {
+         console.log("位置情報の取得に失敗しました。");
+      }
+      return navigator.geolocation.getCurrentPosition(success, error);
     }
   }
   Scratch.extensions.register(new StrictEqualityExtension());
